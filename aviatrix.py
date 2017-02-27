0e7feaaf218fe60b448faaf1bf3ab1790739c800
Rakesh Ranjan <rakeshranjan.568@gmail.com>
2017-02-28 02:38:58 +0530

    Add files via upload

--- aviatrix.py
+++ aviatrix.py
@@ -3,137 +3,126 @@ import sys
 import os
 import requests
 import time
+import re
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
-def readCfgFile(cfgFileName):
+class aviatrixController:
+    def __init__(self, cfg):
+        self.cfg=cfg
+        self.cidr_list = []
+        line_regex = "([a-zA-Z_]+)=(.+)"
+        try:
+            file = open(self.cfg, "r")
+        except IOError as e:
+            print "Cannot open", self.cfg
+            raise
+        for line in file:
+            match = re.match(line_regex, line)
+            if match:
+                setattr(self, match.group(1), match.group(2))
 
-    global BASE_URL
-    global ADMIN_USER
-    global ADMIN_PASS
-    global CID
-    global LICENSE_TOKEN
-    global USER_ACCOUNT_NAME
-    global USER_ACCOUNT_PASSWORD
-    global USER_ACCOUNT_EMAIL
-    global CLOUD_TYPE
-    global AWS_ACCOUNT_NUMBER
-    global AWS_ACCESS_KEY
-    global AWS_SECRET_KEY
-    global VPC_NAME
-    global VPC_REGION
-    global GW_SIZE
-    
-    file = open(cfgFileName, "r")
-    for newLine in file:
-        strlen = len(newLine)
-        newLine = newLine[:strlen - 1]
-        equalIndex = newLine.find("=")
-        key = newLine[:equalIndex]
-        value = newLine[equalIndex+1:].strip()
 
-        if(key == 'base_url'):
-            BASE_URL = value
-        if(key == 'admin_user'):
-            ADMIN_USER = value
-        if(key == 'admin_pass'):
-            ADMIN_PASS = value
-        if(key == 'cid'):
-            CID = value
-        if(key == 'license_token'):
-            LICENSE_TOKEN = value
-        if(key == 'user_account_name'):
-            USER_ACCOUNT_NAME = value
-        if(key == 'user_account_password'):
-            USER_ACCOUNT_PASSWORD = value
-        if(key == 'user_account_email'):
-            USER_ACCOUNT_EMAIL = value
-        if(key == 'cloud_type'):
-            CLOUD_TYPE = value
-        if(key == 'aws_account_number'):
-            AWS_ACCOUNT_NUMBER = value
-        if(key == 'aws_access_key'):
-            AWS_ACCESS_KEY = value
-        if(key == 'aws_secret_key'):
-            AWS_SECRET_KEY = value
-        if(key == 'vpc_name'):
-            VPC_NAME = value
-        if(key == 'vpc_region'):
-            VPC_REGION = value
-        if(key == 'gw_size'):
-            GW_SIZE = value
+    def createSession(self):
+        def createSessionSuccessCb(response_json):
+            try:
+                self.cid = response_json['CID']
+                print "Created new session with CID %s\n" %self.cid
+            except KeyError, e:
+                print "Unable to create session. %s" %str(e)
+                raise
 
+        args = 'action=login&username=' + self.admin_user + '&password=' + self.admin_pass
+        print "Creating new session with username.... "
+        self.do_get(args, createSessionSuccessCb)
+
+
+    def getLicenseInfo(self):
+        def getLicenseInfoSuccessCb(response_json):
+            try:
+                print "License associated with %s\n" %response_json['results']['CustomerID']
+            except KeyError, e:
+                print "Unable to get license info for customer ID: %s.\n%s" %(self.license_token, str(e))
+                raise
+        args = 'CID='+self.cid+'&action=setup_customer_id&customer_id=' + self.license_token
+        print "Getting license info.... "
+        self.do_get(args, getLicenseInfoSuccessCb)
+
+    def setMAxVpcCont(self):
+        max_vpc_num = 2
+        def setMaxVpcContSuccessCb(response_json):
+            try:
+                self.cidr_list = response_json['results']['cidr_list']
+                print "Setting maximum number of VPSs as %d. \nAvailable CIDRs %s\n" %(max_vpc_num, ', '.join(self.cidr_list))
+            except KeyError, e:
+                print "Unable to set VPC containers. %s" %str(e)
+                raise
+
+        args = 'CID='+self.cid+'&action=setup_max_vpc_containers&vpc_num=' + str(max_vpc_num)
+        print "Setting maximum VPCs.... "
+        self.do_get(args, setMaxVpcContSuccessCb)
         
-def createSession():
-    def createSessionSuccessCb(response_json):
-        #print response_json
-        global CID
-        CID = response_json['CID']
-        print "Created new session with CID %s\n" %CID
-    args = 'action=login&username=' + ADMIN_USER + '&password=' + ADMIN_PASS
-    print "Creating new session with username.... "
-    do_get(args, createSessionSuccessCb)
+    def createUserAccount(self):
+        def createUserAccountSuccessCb(response_json):
+            try:
+                print "Account creation successful. %s\n" %response_json['results']
+            except KeyError, e:
+                print "Unable to create User account. %s" %str(e)
+                raise
 
-def getLicenseInfo():
-    def getLicenseInfoSuccessCb(response_json):
-        print "License associalted with %s\n" %response_json['results']['CustomerID'] 
-    args = 'CID='+CID+'&action=setup_customer_id&customer_id=' + LICENSE_TOKEN 
-    print "Getting license info.... "
-    do_get(args, getLicenseInfoSuccessCb)
+        data = {"CID": self.cid, "action": "setup_account_profile", "account_name": self.user_account_name, "account_password": self.user_account_password, "account_email": self.user_account_email, "cloud_type": self.cloud_type, "aws_account_number": self.aws_account_number, "aws_access_key": self.aws_access_key, "aws_secret_key": self.aws_secret_key}
+        print "Creating User account and associating with AWS.... "
+        self.do_post(data, createUserAccountSuccessCb)
 
-def setMAxVpcCont():
-    max_vpc_num = 2
-    def setMaxVpcContSuccessCb(response_json):
-        global CIDR_LIST
-        CIDR_LIST = response_json['results']['cidr_list']
-        print "Setting maximum number of VPSs as %d. \nAvailable CIDRs %s\n" %(max_vpc_num, ', '.join(CIDR_LIST))
-    args = 'CID='+CID+'&action=setup_max_vpc_containers&vpc_num=' + str(max_vpc_num)
-    print "Setting maximum VPCs.... "
-    do_get(args, setMaxVpcContSuccessCb)
 
-def createUserAccount():
-    def createUserAccountSuccessCb(response_json):
-        print "Account creation successful. %s\n" %response_json['results']
+    def createVpcDatacenter(self):
+        def createVpcDatacenterSuccessCb(response_json):
+            try:
+                print "VPC/gateway Creation Successful %s" %response_json['results']
+            except KeyError, e:
+                print "Unable to create VPC/gateway."
+                raise
 
-    data = {"CID": CID, "action": "setup_account_profile", "account_name": USER_ACCOUNT_NAME, "account_password": USER_ACCOUNT_PASSWORD, "account_email": USER_ACCOUNT_EMAIL, "cloud_type": CLOUD_TYPE, "aws_account_number": AWS_ACCOUNT_NUMBER, "aws_access_key": AWS_ACCESS_KEY, "aws_secret_key": AWS_SECRET_KEY}
-    print "Creating User account and associating with AWS.... "
-    do_post(data, createUserAccountSuccessCb)
+        data = {"CID": self.cid, "action": "create_container", "cloud_type": self.cloud_type, "account_name": self.user_account_name, "vpc_name": self.vpc_name, "vpc_reg": self.vpc_region, "vpc_size": self.gw_size, "vpc_net": self.cidr_list[0]}
+        print "Creating VPC with CIDR %s" %cidr_list[0]
+        self.do_post(data, createVpcDatacenterSuccessCb)
 
-def createVpcDatacenter():
-    def createVpcDatacenterSuccessCb(response_json):
-        print "VPC Creation Successful %s" %response_json['results']
 
-    data = {"CID": CID, "action": "create_container", "cloud_type": CLOUD_TYPE, "account_name": USER_ACCOUNT_NAME, "vpc_name": VPC_NAME, "vpc_reg": VPC_REGION, "vpc_size": GW_SIZE, "vpc_net": CIDR_LIST[0]}
-    print "Creating VPC with CIDR %s" %CIDR_LIST[0]
-    do_post(data, createVpcDatacenterSuccessCb)
+    def do_get(self, args, callback):
+        url = self.base_url+'?'+args
+        print url
+        try:
+            response = requests.get(url, verify=False)
+        except Exception, e:
+            print "Unable to establish connection."
+            raise
+        
+        response_json = response.json()
+        if response_json['return'] == True:
+            return callback(response_json)
+        else:
+            print "Failed: %d error - %s" %(response.status_code, str(response_json['reason']))
+            raise Exception("Get request error.")
 
-def do_get(args, callback):
-    url = BASE_URL+'?'+args
-    print url
-    response = requests.get(url, verify=False)
-    #print response.status_code
-    response_json = response.json()
-    #print response_json
-    if response_json['return'] == True:
-        return callback(response_json)
-    else:
-        print "Failed: %d error - %s" %(response.status_code, str(response_json['reason']))
-        exit
-
-def do_post(data, callback):
-    response = requests.post(BASE_URL, data=data, verify=False)
-    response_json = response.json()
-    print response_json
-    if response_json['return'] == True:
-        return callback(response_json)
-    else:
-        print "Failed: %d error - %s" %(response.status_code, str(response_json['reason']))
-        exit
-
-readCfgFile('aviatrix.cfg')
-createSession()
-getLicenseInfo()
-setMAxVpcCont()
-createUserAccount()
-createVpcDatacenter()
+    def do_post(self, data, callback):
+        try:
+            response = requests.post(self.base_url, data=data, verify=False)
+        except Exception, e:
+            print "Unable to establish connection."
+            raise
+        
+        response_json = response.json()
+        print response_json
+        if response_json['return'] == True:
+            return callback(response_json)
+        else:
+            print "Failed: %d error - %s" %(response.status_code, str(response_json['reason']))
+            raise Exception("Post request error.")
+        
+myController = aviatrixController("aviatrix.cfg")
+myController.createSession()
+myController.getLicenseInfo()
+myController.setMAxVpcCont()
+myController.createUserAccount()
+myController.createVpcDatacenter()
 
